482457ea5c3d9d5c4f37f47c8122eb224e9fc308
RacMcT <raccmct@gmail.com>
2021-01-23 15:31:10 -0600

    ham menu edits

--- salon-script.js
+++ salon-script.js
@@ -1,12 +1,11 @@
 //Hamburger Menu JS:
 
-/* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("nav");
-  if (x.className === "nav") {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
     x.className += " responsive";
   } else {
-    x.className = "nav";
+    x.className = "topnav";
   }
 }
-
