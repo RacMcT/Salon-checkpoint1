a2ff985a195f0c42edb0a33352d7a694fbfb4ffc
RacMcT <raccmct@gmail.com>
2021-01-24 12:57:28 -0600

    finish responsive ham menu debug

--- salon-script.js
+++ salon-script.js
@@ -1,8 +1,6 @@
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("nav");
-
+/* Toggle between adding and removing the "responsive" class to 
+topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("nav");
   if (x.className === "nav") {
